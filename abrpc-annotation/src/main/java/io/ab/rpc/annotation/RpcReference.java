facf30d401616c0837ae322062fadb90672f3ebd
sunxinbo <898349230@qq.com>
2023-02-02 23:54:18 +0800

    Initial commit

---
+++ abrpc-annotation/src/main/java/io/ab/rpc/annotation/RpcReference.java
@@ -0,0 +1,59 @@
+package io.ab.rpc.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 消费者注解
+ */
+// 标记到字段上
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Autowired
+public @interface RpcReference {
+
+    /**
+     * 版本号
+     */
+    String version() default "1.0.0";
+    /**
+     * 注册中心类型, 目前的类型包含：zookeeper、nacos、etcd、consul
+     */
+    String registryType() default "zookeeper";
+    /**
+     * 注册地址
+     */
+    String registryAddress() default "127.0.0.1:2181";
+    /**
+     * 负载均衡类型，默认基于ZK的一致性Hash
+     */
+    String loadBalanceType() default "zkconsistenthash";
+    /**
+     * 序列化类型，目前的类型包含：protostuff、kryo、json、jdk、hessian2、fst
+     */
+    String serializationType() default "protostuff";
+    /**
+     * 超时时间
+     */
+    long timeout() default 5000;
+    /**
+     * 是否异步执行
+     */
+    boolean async() default false;
+    /**
+     * 是否单项调用
+     */
+    boolean oneway() default false;
+    /**
+     * 服务消费者默认的代理
+     */
+    String proxy() default "jdk";
+    /**
+     * 默认分组
+     */
+    String group() default "";
+}
